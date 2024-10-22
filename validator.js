e7ba5a07847d27051129546225fc3ab327adc5e5
Minh <achillesback0511@yahoo.com>
2024-10-22 18:25:26 +0700

    second

--- validator.js
+++ validator.js
@@ -1,12 +1,25 @@
 //Doi tuong `Validator`
 function Validator(options) {
+  var selectorRules = {};
+
   //Ham thuc hien Validate
   function validate(inputElement, rule) {
     //value: inputElement.value
     //test func: rule.test
-    var errorMessage = rule.test(inputElement.value);
-    var errorElement =
-      inputElement.parentElement.querySelector(options.errorSelector);
+    var errorMessage;
+    var errorElement = inputElement.parentElement.querySelector(
+      options.errorSelector
+    );
+
+    //Lay ra cac rules cua selector
+    var rules = selectorRules[rule.selector];
+
+    //Lap qua tung rule & kiem tra
+    //Neu co loi thi dung viec kiem tra
+    for (var i = 0; i < rules.length; ++i) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
+    }
 
     if (errorMessage) {
       errorElement.innerText = errorMessage;
@@ -15,13 +28,55 @@ function Validator(options) {
       errorElement.innerText = "";
       inputElement.parentElement.classList.remove("invalid");
     }
+
+    return !errorMessage;
   }
 
   //Lay element cua form can validate
   var formElement = document.querySelector(options.form);
 
   if (formElement) {
+    //Khi submit form
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      var isFormValid = true;
+
+      //Lap qua tung rules va validate
+      options.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        var isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+
+      if (isFormValid) {
+        //Truong hop submit voi javascript
+        if (typeof options.onSubmit === 'function') {
+          var enableInputs = formElement.querySelectorAll('[name]');
+
+          var formValues = Array.from(enableInputs).reduce(function (values, input) {
+            return (values[input.name] = input.value) && values;
+          }, {});
+          options.onSubmit(formValues);
+        } 
+        //Truong hop submit voi hanh vi mac dinh
+        else {
+          formElement.submit();
+        }
+      }
+    };
+
+    //Lap qua moi rule va xu ly (Lang nghe su kien, blur, input, ...)
     options.rules.forEach(function (rule) {
+      //Luu lai cac rules trong moi input
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
       var inputElement = formElement.querySelector(rule.selector);
       if (inputElement) {
         //Xu ly truong hop blur ra khoi input
@@ -31,8 +86,9 @@ function Validator(options) {
 
         //Xu ly moi khi nguoi dung nhap
         inputElement.oninput = function () {
-          var errorElement =
-            inputElement.parentElement.querySelector(options.errorSelector);
+          var errorElement = inputElement.parentElement.querySelector(
+            options.errorSelector
+          );
           errorElement.innerText = "";
           inputElement.parentElement.classList.remove("invalid");
         };
@@ -45,31 +101,46 @@ function Validator(options) {
 //Nguyen tac cua cac rules
 //1. Khi co loi => Tra ra message loi
 //2. Khi hop le => Khong tra ra cai gi ca (undifined)
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
-      return value.trim() ? undefined : "Vui lòng nhập trường này";
+      return value.trim() ? undefined : message || "Vui lòng nhập trường này";
     },
   };
 };
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       var regex =
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return regex.test(value) ? undefined : "Trường này phải là Email";
+      return regex.test(value)
+        ? undefined
+        : message || "Trường này phải là Email";
     },
   };
 };
 
-Validator.minLength = function (selector, min) {
-    return {
-      selector: selector,
-      test: function (value) {
-        return value.length >= min ? undefined: `Vui lòng nhập tối thiểu ${min} kí tự`
-      },
-    };
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+    },
   };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || "Giá trị nhập vào không chính xác";
+    },
+  };
+};
